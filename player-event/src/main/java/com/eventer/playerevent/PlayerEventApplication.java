0864fbc54acfee0b83f3ef73fbfd0944cc37a24e
Uros Poznan <poznanuros@gmail.com>
2021-10-28 00:15:46 +0200

    Initial commit

---
+++ player-event/src/main/java/com/eventer/playerevent/PlayerEventApplication.java
@@ -0,0 +1,13 @@
+package com.eventer.playerevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerEventApplication.class, args);
+	}
+
+}
---
+++ player-event/src/test/java/com/eventer/playerevent/PlayerEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventer.playerevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
