f862cb95c0d8e56f0347e57ca4c109460702a6ee
Uroš Poznan <uros.poznan@mozzartbet.com>
2022-02-15 16:39:19 +0100

    problem sa gitignoreom - brisanje .idea fajlova

--- wallet/src/main/java/com/eventer/wallet/amqp/rabbit/PaymentRequestListener.java
+++ wallet/src/main/java/com/eventer/wallet/amqp/rabbit/PaymentRequestListener.java
@@ -21,16 +21,30 @@ public class PaymentResponseListener {
     @Autowired
     PaymentResponseSender sender;
 
-    @Async("payment-request-executor")
+    @Async("payin-request-executor")
     @RabbitListener(queues = Queues.WALLET_PAYIN_REQUEST_QUEUE)
     public void receiveWalletPayinResponse(UserTransaction pmt) {
         log.info("Recieved Message From RabbitMQ: " + pmt);
-                PaymentResponseDTO resp = customerService.processPaymentRequest(pmt);
-                try{
-                    sender.send(resp);
-                } catch(Exception e){
-                    log.info("STAVI RETRY ZA OPTIMISTIK LOK");
-                }
+        try {
+            PaymentResponseDTO resp = customerService.processPaymentRequest(pmt);
+            sender.sendPayinResponse(resp);
+        } catch (Exception e) {
+            log.info("STAVI RETRY ZA OPTIMISTIK LOK");
+        }
+
+    }
+
+    @Async("withdraw-request-executor")
+    @RabbitListener(queues = Queues.WALLET_WITHDRAW_REQUEST_QUEUE)
+    public void receiveWalletWithdrawResponse(UserTransaction pmt) {
+        log.info("Recieved Message From RabbitMQ: " + pmt);
+        try {
+            pmt.setAmount(pmt.getAmount().negate());
+            PaymentResponseDTO resp = customerService.processPaymentRequest(pmt);
+            sender.sendWithdrawResponse(resp);
+        } catch (Exception e) {
+            log.info("STAVI RETRY ZA OPTIMISTIK LOK");
+        }
 
     }
 
