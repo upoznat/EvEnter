84740a2e9a96ff3c4dc87890f4fa049413d347af
Uroš Poznan <uros.poznan@mozzartbet.com>
2022-02-16 23:28:34 +0100

    sitne izmene

---
+++ wallet/${project.build.directory}/classes/db/init/create_schema.sql
@@ -0,0 +1,6 @@
+create user wallet identified by "wallet!";
+grant unlimited tablespace to wallet with admin option;
+grant connect to wallet with admin option;
+grant resource to wallet with admin option;
+grant authenticateduser to wallet with admin option;
+grant create view to wallet;
